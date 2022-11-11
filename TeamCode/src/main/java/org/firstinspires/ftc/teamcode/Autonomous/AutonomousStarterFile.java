d2b6607f193407fca40c5d99a9c88b50bbdb8f70
johnylamw <jlwu2231@cs.washington.edu>
2022-11-10 16:29:39 -0800

    created a starter file for autonomous.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutonomousStarterFile.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Disabled
+@Autonomous(name="Autonomous (Change it!)", group="Testing")
+public class AutonomousStarterFile extends LinearOpMode {
+
+    // Declare OpMode Members (Motors, Servos.. etc)
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Initialize Mode
+
+
+        // Wait until (play) is pressed
+        waitForStart();
+
+        // Insert Autonomous Code below:
+
+    }
+}
