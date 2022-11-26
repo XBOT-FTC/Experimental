6ac0a8ae0203dbab93c7677e99ff5060dfaf0c04
Jeremy Calvert <jcalvert@gmail.com>
2022-11-26 12:18:41 -0800

    First version had SO MANY BUGS!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/Grabber.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/Grabber.java
@@ -2,26 +2,24 @@ package org.firstinspires.ftc.teamcode.lib;
 
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.Gamepad;
-import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class Grabber {
 
-    private DcMotor grabberMotor = null;
+    private Servo grabberMotor = null;
 
-    public Grabber(Servo slideMotor) {
-        this.slideMotor = slideMotor;
+    public Grabber(Servo grabberMotor) {
+        this.grabberMotor = grabberMotor;
     }
 
 
     public void grab(Gamepad gamepad, Telemetry telemetry) {
-        if (gamepad2.left_bumper) {
-            grabber.setPosition(0);
-        } else if (gamepad2.right_bumper) {
-            grabber.setPosition(0.3);
+        if (gamepad.left_bumper) {
+            grabberMotor.setPosition(0);
+        } else if (gamepad.right_bumper) {
+            grabberMotor.setPosition(0.3);
         }
-        telemetry.addData("Grabber Position: ", grabber.getPosition());
+        telemetry.addData("Grabber Position: ", grabberMotor.getPosition());
     }
-
 }
