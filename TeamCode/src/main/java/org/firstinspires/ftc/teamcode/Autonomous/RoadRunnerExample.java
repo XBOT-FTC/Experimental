34a0e3187179041f4015e8a12595e518dca41272
johnylamw <jlwu2231@cs.washington.edu>
2022-12-17 15:42:59 -0800

    implemented roadrunner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RoadRunnerExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RoadRunnerExample.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.Autonomous;
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -21,42 +22,16 @@ public class RoadRunnerExample extends LinearOpMode {
         Grabber grabber = new Grabber(hardwareMap.servo.get(RobotConstants.GRABBER), Servo.Direction.REVERSE);
 
         Trajectory myTrajectory = drive.trajectoryBuilder(new Pose2d())
-                .forward(50)
-                .turn(Math.toRadians(-135))
+                .splineTo(new Vector2d(36, 12), Math.toRadians(270))
                 // Slide Up
-                .forward(5)
-                // Grab: Open
-                .back(5)
-                // Slide Down
-                .turn(Math.toRadians(-135))
-                .forward(20)
-                // Slide Up
+                .splineTo(new Vector2d(31, 12), Math.toRadians(135))
+                // Grab: Close
+                .lineToLinearHeading(new Pose2d(36, 12, Math.toRadians(0)))
+                .lineToLinearHeading(new Pose2d(56, 12, Math.toRadians(0)))
                 // Slide Down
                 // Grab: Open
-                // Slide Down
-                // Slide Up
-                .back(20)
-                .turn(Math.toRadians(135))
-                .forward(5)
-                // Grab: Open
-                .back(5)
-                // Slide Down
-                .turn(Math.toRadians(-135))
-                .forward(20)
-                // Slide Up
-                // Slide Down
-                // Grab: Open
-                // Slide Down
-                // Slide Up
-                .back(20)
-                .turn(Math.toRadians(135))
-                .forward(5)
-                // Grab: Open
-                .back(5)
-                // Slide Down
-                .turn(Math.toRadians(-135))
-                // Park at designated spot:
-                .strafeLeft(20)
+                .lineToLinearHeading(new Pose2d(36, 12, Math.toRadians(0)))
+//              .lineToLinearHeading(new Pose2d(31, 12, Math.toRadians(135)))
                 .build();
 
         waitForStart();
