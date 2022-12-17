aa18a1f19e771449783adfb695845b959fcac091
Stephanie <sw7859305@gmail.com>
2022-12-17 13:12:44 -0800

    changed kV, kA,max velocity, and max accel

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/Roadrunner/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/Roadrunner/drive/DriveConstants.java
@@ -55,8 +55,8 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 1.0 / rpmToVelocity(MAX_RPM);
-    public static double kA = 0;
+    public static double kV = 0.0164986;
+    public static double kA = 0.0019999999999;
     public static double kStatic = 0;
 
     /*
@@ -66,8 +66,8 @@ public class DriveConstants {
      * small and gradually increase them later after everything is working. All distance units are
      * inches.
      */
-    public static double MAX_VEL = 64.3159414;
-    public static double MAX_ACCEL = 64.3159414;
+    public static double MAX_VEL = 51.452;
+    public static double MAX_ACCEL = 51.452;
     public static double MAX_ANG_VEL = Math.toRadians(180);
     public static double MAX_ANG_ACCEL = Math.toRadians(180);
 
